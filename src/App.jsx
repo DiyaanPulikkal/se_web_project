3798565aab1f4868716d2ea47ef29f2c570888e4
Diyaan Pulikkal <66010991@kmitl.ac.th>
2024-09-12 20:37:15 +0700

    r

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,23 @@ import Contact from "./Contact/Contact.jsx";
 function App() {
   return (
     <>
-    
-    <Router>
-    <div style={{backgroundColor: '#0B2F9F', display: 'flex'}}>
-      <NavBar />
-    </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/program" element={<Program />} />
-        <Route path="/admission" element={<Admission />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <header>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/program" element={<Program />} />
+            <Route path="/admission" element={<Admission />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </header>
+
+
     </>
-    
+
   )
 }
 
