2cd793aa66e73b02d2b380c7fa4ecd7c016f16e0
Diyaan Pulikkal <66010991@kmitl.ac.th>
2024-09-14 15:03:32 +0700

    a

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,21 @@
 import style from './Footer.module.css'
+import phoneIcon from './phone-call.png'
+import emailIcon from './email.png'
 function Footer() {
   return (
     <footer className={style.mainFooter}>
-      <p></p>
+      <h2><bold>King Mongkut's Institute of Techonology, Ladkrabang</bold></h2>
+      <div className={style.container}>
+        <div className={style.footerLeft}>
+          <h3>Address</h3>
+          <p>King Mongkut Institute of Technology <br />Ladkrabang 1, Chalongkrung 1 Alley, <br />Lat Krabang, Bangkok 10520, Thailand</p>
+          <br />
+          <img className={style.icons} src={phoneIcon} alt="phone" /> xxx-xxx-xxxx
+          <br />
+          <br /> 
+          <img className={style.icons} src={emailIcon} alt="email" /> xxxxx@xxxxx.xxx
+        </div>
+      </div>
     </footer>
   );
 }
