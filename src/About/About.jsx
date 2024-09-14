d971d69cf0b27d579c8e43ff1a4081620688c4d7
phathompol <phathompols@gmail.com>
2024-09-14 11:06:19 +0700

    edit about
    lol diyaan

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,8 +1,32 @@
-import "./About.module.css";
+import style from "./About.module.css";
 function About(){
 
     return(
-        <h1>About</h1>
+        <>
+        
+        <div>
+            <h1>What is software Engineering?</h1>
+            <div className={style.textExplainArea}>
+                <p>Software engineering (SE) is an engineering discipline concerning all aspects of software production, including software analysis, design, development, testing, and deployment. SE requires profound abstract and logical thinking and the application of mathematics, logic, and computer science in order to produce efficient and reliable software with the available resources.</p>
+            </div>
+        </div>
+        
+        <div>
+            <h1>Why study Software Engineer?</h1>
+            <div className={style.textExplainArea}>
+                <p>It is hard to overstate the ubiquity of software nowadays. Every computer system is governed by software. Almost every human activity involves software in some form. Undoubtedly software industry is one of the largest and fastest growing industries in the world. Consequently, skilled software engineers are in high demand worldwide. As software becomes more and more complex, the programming skills and the rudimentary knowledge of software engineering that students obtained from traditional computer science and computer engineering curriculums are insufficient. The development of real-world software applications requires the skills in analysing the problem domain and the customer's requirement and the skills in designing the software from the topmost level down to the implementation level. Moreover, a software engineer must be able to use proper tools, techniques, and methodologies in order to produce the software in an efficient manner.</p>
+            </div>
+        </div>
+
+        <div>
+            <h1>Why KMITL?</h1>
+            <div className={style.textExplainArea}>
+                <p>KMITL’s Software Engineering program stands out for its strong technical foundation, practical learning approach, and industry connections. Known as one of Thailand’s top engineering institutes, KMITL offers a well-rounded curriculum covering core areas of software development and emerging fields like AI and cybersecurity. Students benefit from modern labs, industry-sponsored projects, and internship opportunities, all of which provide real-world experience. With international partnerships, a vibrant student community, and a focus on innovation, KMITL fosters an environment conducive to both academic excellence and career readiness, making it a competitive choice for aspiring software engineers</p>
+            </div>
+        </div>
+
+
+        </>
     );
 }
 
