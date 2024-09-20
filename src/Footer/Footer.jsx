ea6a5110d84e1e1ac78bf72eb641dd9208cd4b17
Diyaan Pulikkal <66010991@kmitl.ac.th>
2024-09-20 10:41:29 +0700

    a

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import style from './Footer.module.css'
 import phoneIcon from './phone-call.png'
 import emailIcon from './email.png'
@@ -10,13 +11,27 @@ function Footer() {
           <h3>Address</h3>
           <p>King Mongkut Institute of Technology <br />Ladkrabang 1, Chalongkrung 1 Alley, <br />Lat Krabang, Bangkok 10520, Thailand</p>
           <br />
-          <img className={style.icons} src={phoneIcon} alt="phone" /> xxx-xxx-xxxx
+          <img className={style.icons} src={phoneIcon} alt="phone" /> 02-329-8000
           <br />
           <br /> 
-          <img className={style.icons} src={emailIcon} alt="email" /> xxxxx@xxxxx.xxx
+          <img className={style.icons} src={emailIcon} alt="email" /> siie@kmitl.ac.th
         </div>
         <div className={style.footerRight}>
-          <div><h3>Our Topics</h3></div>
+          <div>
+            <h3>Our Topics</h3>
+            <Link to="/about">About</Link><br />
+            <Link to="/program">Program</Link><br />
+          </div>
+          <div>
+            <h3>&nbsp;</h3>
+            <Link to="/activities">Activities</Link><br />
+            <Link to="/contact">Contact</Link><br />
+          </div>
+          <div>
+            <h3>&nbsp;</h3>
+            <Link to="/activities">Admission</Link><br />
+            <Link to="/login">Login</Link><br />
+          </div>
           
         </div>
       </div>
